9e2723c9cd89f04f5e4e6656051083ded11e72dd
SiddharthaBhupathi <59827243+SiddharthaBhupathi@users.noreply.github.com>
2020-01-31 13:57:11 +0530

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs
+{
+    class square
+    {
+        static void Main(String[]args)
+        {
+            {
+                int i, j;
+                int N = 10;
+                for (i = 1; i <= N; i++)
+                {
+                    for (j = 1; j <= N; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+
+            }
+        }
+    }
+}
